a5a9489d94e566d6d6e752a9b6f0f9d19550ae1c
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-13 04:31:47 +0800

    feat: Add password reset functionality
    This commit adds a new method `getResetPasswordToken` to the `userSchema` in the `user.js` file. This method generates a password reset token using the `crypto` module and sets the `resetPasswordToken` field in the user document. It also sets the `resetPasswordExpire` field to indicate the expiration time of the token. This enhancement allows users to request a password reset and receive a unique token for verification.

--- BackEnd/models/user.js
+++ BackEnd/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -51,6 +52,24 @@ userSchema.methods.getJwtToken = function () {
 // Compare user password 
 userSchema.methods.comparePassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
+}
+
+// Generate password reset token
+userSchema.methods.getResetPasswordToken = function () {
+
+    // Generate token 
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hash and set to resetPasswordToken field
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    // Set token expire time
+    this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+    return resetToken;
 }
 
 module.exports = mongoose.model('User', userSchema);
