4bc3626aeee0ac3b5d90fae844f29230ce64ad70
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-14 00:52:56 +0800

    feat: Add endpoints for order management
    This commit adds new endpoints for order management in the `orderController.js` file. It includes the following changes:
    - Added a route `/api/v1/order/:id` to get a single order by ID
    - Added a route `/api/v1/me/order` to get the current user's orders
    - Added a route `/api/v1/admin/order` to get all orders (for admin use)
    
    These enhancements improve the application by providing better order management capabilities for users and administrators.

--- BackEnd/controllers/orderController.js
+++ BackEnd/controllers/orderController.js
@@ -33,3 +33,39 @@ module.exports.newOrder = catchAsyncError(async (req, res, next) => {
         order
     });
 });
+
+// Get single order => /api/v1/order/:id
+module.exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
+    const { id } = req.params;
+    const order = await Order.findById(id).populate(
+        'user',
+        'name email'
+    );
+
+    if (!order) return next(new ErrorHandler('Order not found', 404));
+
+    res.status(200).json({
+        success: true,
+        order
+    });
+});
+
+// Get current user order => /api/v1/me/order
+module.exports.myOrders = catchAsyncError(async (req, res, next) => {
+    const order = await Order.find({ user: req.user._id });
+
+    res.status(200).json({
+        success: true,
+        order
+    });
+});
+
+// Get all orders - ADMIN => /api/v1/admin/order
+module.exports.allOrders = catchAsyncError(async (req, res, next) => {
+    const order = await Order.find();
+
+    res.status(200).json({
+        success: true,
+        order
+    });
+});
--- BackEnd/routes/order.js
+++ BackEnd/routes/order.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const { newOrder } = require('../controllers/orderController');
+const { newOrder, getSingleOrder, myOrders } = require('../controllers/orderController');
 const { isAuthenticatedUser } = require('../middlewares/auth');
 const router = express.Router();
 
 router
     .route('/order/new')
     .post(isAuthenticatedUser, newOrder);
+
+router
+    .route('/order/:id')
+    .get(isAuthenticatedUser, getSingleOrder);
+
+router
+    .route('/me/orders')
+    .get(isAuthenticatedUser, myOrders);
 
 module.exports = router;
