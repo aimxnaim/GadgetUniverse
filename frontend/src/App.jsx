32133574830b1e4bdefc871d0c7615a97c7e6cc7
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-18 01:07:32 +0800

    feat: Update App component to use react-router-dom for routing
    This commit updates the App component in the frontend codebase to use the react-router-dom library for routing. It replaces the static rendering of the Home component with a dynamic routing setup using the BrowserRouter, Routes, and Route components. This enhancement improves the application by enabling navigation between different pages and improving the overall user experience.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,23 @@ import './App.css';
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from './components/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 function App() {
 
   return (
-    <div className='App'>
-      <Header />
-      <Home />
-      <Footer />
-    </div>
+    <Router>
+      <div className='App'>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
