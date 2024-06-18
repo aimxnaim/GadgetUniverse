d33ad7528ee305076e74b824b2bf4dedd5fe152e
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-18 22:30:33 +0800

    refactor: clean code in Home.jsx

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -11,6 +11,7 @@ const Home = () => {
     useEffect(() => {
         if (isError) toast.error(error?.data?.message)
     }, [isError]);
+
     if (isLoading) return <Loading />
     return (
         <>
