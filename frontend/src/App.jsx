3a6ed3a41eca76765b28d11cf93a72cebea3acdd
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-18 22:30:02 +0800

    ```text feat: Add route for product details page
    This commit adds a new route for the product details page in the App component. The route path '/products/:id' is mapped to the ProductDetails component. This enhancement enables users to view detailed information about a specific product by navigating to the corresponding URL.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/layout/Footer";
 import Home from './components/Home';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
+import ProductDetails from './components/product/ProductDetails';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />}></Route>
+            <Route path='/products/:id' element={<ProductDetails />}></Route>
           </Routes>
         </div>
         <Footer />
