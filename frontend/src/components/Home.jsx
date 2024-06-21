05ba13edfe2a7b867e7e7ffc8c85bc9c7ff11ac9
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-22 01:13:15 +0800

    feat: Add Filter component for filtering products
    This commit adds the Filter component to the frontend codebase. The Filter component allows users to filter products based on price range. It provides input fields for users to enter minimum and maximum prices, and a "GO" button to apply the filter. When the filter is applied, the URL is updated with the corresponding query parameters, allowing users to bookmark or share the filtered results. This enhancement improves the user experience by providing a convenient way to narrow down product search results.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,12 +6,18 @@ import Loading from './layout/Loader'
 import toast from 'react-hot-toast'
 import CustomPagination from './layout/CustomPagination'
 import { useSearchParams } from 'react-router-dom'
+import Filter from './layout/Filter'
 
 const Home = () => {
     let [searchParams] = useSearchParams();
     const page = Number(searchParams.get('page')) || 1;
     const keyword = searchParams.get('keyword') || '';
+    const min = searchParams.get('min');
+    const max = searchParams.get('max');
     const params = { page, keyword };
+
+    min !== null && (params.min = min);
+    max !== null && (params.max = max);
 
     const { data, isLoading, error, isError } = useGetProductsQuery(params);
 
@@ -28,7 +34,7 @@ const Home = () => {
             <div className="row">
                 {keyword &&
                     <div className="col-6 col-md-3 mt-5">
-                        <p>Filters</p>
+                        <Filter />
                     </div>
                 }
                 <div className={keyword ? "col-6 col-sm-6 col-md-9" : "col-6 col-sm-6 col-md-12"}>
@@ -43,8 +49,8 @@ const Home = () => {
 
                     <section id="products" className="mt-5">
                         <div className="row">
-                            {data?.products?.map((product) => (
-                                <ProductItem key={product.id} product={product} columnSize={columnSize} />
+                            {data?.products?.map((product, index) => (
+                                <ProductItem key={product.id || index} product={product} columnSize={columnSize} />
                             ))}
 
                         </div>
