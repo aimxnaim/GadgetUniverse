64a69e230161c5655aeb655d84d9b8f30f345b04
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-22 00:16:21 +0800

    feat: Add Search component for searching products
    This commit adds the Search component to the frontend codebase. The Search component allows users to search for products by entering a keyword. When the form is submitted, the keyword is used to navigate to the corresponding search results page. This enhancement improves the user experience by providing a convenient way to search for products within the application.

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -10,25 +10,41 @@ import { useSearchParams } from 'react-router-dom'
 const Home = () => {
     let [searchParams] = useSearchParams();
     const page = Number(searchParams.get('page')) || 1;
-    const params = { page };
+    const keyword = searchParams.get('keyword') || '';
+    const params = { page, keyword };
+
     const { data, isLoading, error, isError } = useGetProductsQuery(params);
 
     useEffect(() => {
         if (isError) toast.error(error?.data?.message)
     }, [isError, error]);
 
+    const columnSize = keyword ? 4 : 3;
+
     if (isLoading) return <Loading />
     return (
         <>
             <MetaData title={`Buy Best Products Online`}></MetaData>
             <div className="row">
-                <div className="col-12 col-sm-6 col-md-12">
-                    <h1 id="products_heading" className="text-secondary">Latest Products</h1>
+                {keyword &&
+                    <div className="col-6 col-md-3 mt-5">
+                        <p>Filters</p>
+                    </div>
+                }
+                <div className={keyword ? "col-6 col-sm-6 col-md-9" : "col-6 col-sm-6 col-md-12"}>
+                    <h1 id="products_heading" className="text-secondary">
+                        {
+                            keyword
+                                ? `${data?.products?.length} Products found with keyword ${keyword}`
+                                : 'Latest Products'
+                        }
+
+                    </h1>
 
                     <section id="products" className="mt-5">
                         <div className="row">
                             {data?.products?.map((product) => (
-                                <ProductItem key={product.id} product={product} />
+                                <ProductItem key={product.id} product={product} columnSize={columnSize} />
                             ))}
 
                         </div>
