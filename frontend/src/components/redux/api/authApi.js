1ee0a4d0f5eeffa9904ce92e7d8a06fea4a840ff
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-22 17:36:09 +0800

    feat: Add logout functionality to user dropdown menu in Header component

--- frontend/src/components/redux/api/authApi.js
+++ frontend/src/components/redux/api/authApi.js
@@ -39,7 +39,10 @@ export const authApi = createApi({
                 }
             }
         }),
+        logout: builder.query({
+            query: () => '/logout',
+        })
     })
 })
 
-export const { useLoginMutation, useRegisterMutation } = authApi;
+export const { useLoginMutation, useRegisterMutation, useLazyLogoutQuery } = authApi;
