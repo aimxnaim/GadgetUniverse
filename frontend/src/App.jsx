847dda708ee6dd534a2341b5bb6b81b1617e97ec
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-22 22:03:19 +0800

    feat: Add update profile functionality
    This commit adds the update profile functionality to the frontend codebase. It includes the implementation of the UpdateProfile component, which allows users to update their profile information such as name and email. The component utilizes the useUpdateProfileMutation hook from the userApi to handle the update profile functionality. This enhancement improves the user experience by providing a seamless way for users to modify their profile information within the application.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Header from './components/layout/Header';
 import Profile from './components/user/Profile';
+import UpdateProfile from './components/user/UpdateProfile';
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
             <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
             <Route path='/me/profile' element={<Profile />} />
+            <Route path='/me/update_profile' element={<UpdateProfile />} />
           </Routes>
         </div>
         <Footer />
