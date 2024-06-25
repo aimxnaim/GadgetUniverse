6cb7ac9a0da257d6389640a666a679b9ede502a0
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-25 22:39:45 +0800

    feat: Refactor ProtectedRoutes component to remove unnecessary code

--- frontend/src/components/auth/ProtectedRoutes.jsx
+++ frontend/src/components/auth/ProtectedRoutes.jsx
@@ -1,19 +1,10 @@
-import React, { useEffect, useRef } from 'react'
+import React from 'react'
 import { useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
 import Loader from '../layout/Loader'
-import toast from 'react-hot-toast'
 
 const ProtectedRoutes = ({ admin, children }) => {
     const { isAuthenticated, user, loading } = useSelector((state) => state.auth)
-    const toastShownRef = useRef(false)
-
-    useEffect(() => {
-        if (user?.role !== 'admin' && admin && !toastShownRef.current) {
-            toast.error('You are not authorized to access this route')
-            toastShownRef.current = true
-        }
-    }, [user, admin])
 
     if (loading) {
         return <Loader />
