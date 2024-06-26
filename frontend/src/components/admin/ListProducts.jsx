3d4b48b8f913d6ccf6dcefce73c6d566e8c674c6
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-06-26 15:53:04 +0800

    feat: Add deleteProduct mutation and useDeleteProductMutation

--- frontend/src/components/admin/ListProducts.jsx
+++ frontend/src/components/admin/ListProducts.jsx
@@ -4,25 +4,35 @@ import toast from 'react-hot-toast'
 import Loader from '../layout/Loader'
 import { MDBDataTable } from 'mdbreact'
 import MetaData from '../layout/MetaData'
-import { useGetAdminProductsQuery } from '../../actions/api/productsApi'
+import { useDeleteProductMutation, useGetAdminProductsQuery } from '../../actions/api/productsApi'
 import AdminLayout from '../layout/AdminLayout'
 
 const ListProducts = () => {
 
     const { data, isLoading, error } = useGetAdminProductsQuery()
+    const [
+        deleteProduct,
+        { isLoading: deleteProductLoading, isSuccess, error: deleteProductError }
+    ] = useDeleteProductMutation()
 
     useEffect(() => {
 
         error && toast.error(error?.data?.message)
+        deleteProductError && toast.error(deleteProductError?.data?.message)
+        isSuccess && toast.success('Product deleted successfully')
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [error])
+    }, [error, deleteProductError, isSuccess])
 
     if (isLoading) return <Loader />
 
     function capitalizeFirstLetter(string) {
         if (!string) return '';
         return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const deleteProductHandler = (productId) => {
+        deleteProduct(productId)
     }
 
     const setProducts = () => {
@@ -65,8 +75,20 @@ const ListProducts = () => {
                         <Link to={`/admin/products/${product?._id}/upload_images`} className='btn btn-outline-success ms-2'>
                             <i className='fa fa-image'></i>
                         </Link>
-                        <button className='btn btn-outline-danger ms-2'>
-                            <i className='fa fa-trash'></i>
+                        <button
+                            className='btn btn-outline-danger ms-2'
+                            onClick={() => deleteProductHandler(product?._id)}
+                            disabled={deleteProductLoading}
+                        >
+                            {
+                                deleteProductLoading
+                                    ? (
+                                        <>
+                                            <span className="spinner-border spinner-border-sm" aria-hidden="true"></span>
+                                            <span role="status"></span>
+                                        </>
+                                    ) : <i className='fa fa-trash'></i>}
+
                         </button>
                     </>
                 )
