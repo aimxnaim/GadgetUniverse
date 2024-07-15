af946ef1e5e5215a2db36cee923d271697820d54
Aiman Naim <76118353+aimxnaim@users.noreply.github.com>
2024-07-15 23:44:50 +0800

    refactor: Update ProductItem component layout and lazy load images
    This commit refactors the ProductItem component to improve its layout and performance. The component now uses a responsive grid layout with the column size specified by the 'columnSize' prop. Additionally, the images in the component are now lazy loaded using the 'loading' attribute set to 'lazy'. This change enhances the user experience by optimizing page load time and improving the overall visual presentation of the product items.

--- frontend/src/components/product/ProductItem.jsx
+++ frontend/src/components/product/ProductItem.jsx
@@ -4,43 +4,75 @@ import StarRatings from 'react-star-ratings'
 
 const ProductItem = ({ product, columnSize }) => {
     return (
-        <div className={`col-sm-12 col-md-6 col-lg-${columnSize} my-3`} >
-            <div className="card">
-                <img
-                    className="card-img-top mx-auto"
-                    src={product?.images[0]
-                        ? product?.images[0].url
-                        : '/images/default_product.png'}
-                    alt={product?.name}
-                />
-                <div
-                    className="card-body align-items-center d-flex justify-content-center flex-column"
-                >
-                    <h5 className="card-title">
-                        <Link to={`products/${product?._id}`}>{product?.name}</Link>
-                    </h5>
-                    <div className="ratings mt-auto d-flex">
-                        <StarRatings
-                            rating={product?.ratings}
-                            starRatedColor="#ffb829"
-                            numberOfStars={5}
-                            name='rating'
-                            starDimension='20px'
-                            starSpacing='1px'
-                        />
-                        <span id="no_of_reviews" className="pt-2 ps-2">
-                            {" "}
-                            ({product?.numOfReviews})
-                        </span>
-                    </div>
-                    <p className="card-text mt-2">RM {product?.price}</p>
-                    <Link
-                        to={`products/${product?._id}`}
-                        id="view_btn"
-                        className="button-card"
-                    >
-                        View Details
+        <div className={`col-3 col-lg-${columnSize} `} >
+            <div className="product-card position-relative">
+                <div className="wishlist-icon position-absolute">
+                    <Link>
+                        <img src="/images/youtube/wish.svg" alt="wishlist" />
                     </Link>
+                </div>
+                <div className="product-image text-center ">
+                    <img
+                        className="img-fluid"
+                        src={product?.images[0]
+                            ? product?.images[0].url
+                            : '/images/default_product.png'}
+                        alt={product?.name}
+                        loading="lazy"
+                        style={{ height: '220px', width: '200px' }}
+                    />
+                    <img
+                        className="img-fluid mx-auto"
+                        src={product?.images[1]
+                            ? product?.images[1].url
+                            : '/images/default_product.png'}
+                        alt={product?.name}
+                        loading="lazy"
+                        style={{ height: '220px', width: '200px' }}
+                    />
+                </div>
+                <div className="product-details">
+                    <div className="align-items-center d-flex justify-content-center flex-column">
+                        <h6 className='product-card-brand py-2'>{product?.seller}</h6>
+                        <h5 className="product-card-title">
+                            <Link to={`products/${product?._id}`}>{product?.name}</Link>
+                        </h5>
+                        <div className="ratings mt-auto d-flex">
+                            <StarRatings
+                                rating={product?.ratings}
+                                starRatedColor="#ffb829"
+                                numberOfStars={5}
+                                name='rating'
+                                starDimension='20px'
+                                starSpacing='1px'
+                            />
+                            <span id="no_of_reviews" className="pt-2 ps-2">
+                                {" "}
+                                ({product?.numOfReviews})
+                            </span>
+                        </div>
+                        <p className="product-card-text mt-2">RM {product?.price}</p>
+                        <Link
+                            to={`products/${product?._id}`}
+                            id="view_btn"
+                            className="button-card mb-3"
+                        >
+                            View Details
+                        </Link>
+                    </div>
+                </div>
+                <div className="action-bar position-absolute">
+                    <div className="d-flex flex-column gap-10">
+                        <Link>
+                            <img src="/images/youtube/prodcompare.svg" alt="compare" />
+                        </Link>
+                        <Link>
+                            <img src="/images/youtube/view.svg" alt="view" />
+                        </Link>
+                        <Link>
+                            <img src="/images/youtube/add-cart.svg" alt="addToCart" />
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div >
