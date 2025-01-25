8b9c48497387ed90df9fce679e755815c8163bd6
aimxnaim <aimxnaim@gmail.com>
2025-01-25 17:27:47 +0800

    refactor: 🐍Update database connection logic in seeder.js for environment-specific URIs

--- BackEnd/seed/seeder.js
+++ BackEnd/seed/seeder.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 const Product = require('../models/product');
 const products = require('./data');
+require('dotenv').config({ path: 'BackEnd/config/.env' });
 
 const seedProducts = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://aimxnaim:zxoUbmVEGchx0IzG@gadgetuniverse.niqzb5t.mongodb.net/gadgetUniverse?retryWrites=true&w=majority&appName=gadgetUniverse`);
+        let DB_URI = "";
+
+        if (process.env.NODE_ENV === 'DEVELOPMENT') DB_URI = process.env.DB_LOCAL_URI;
+        else if (process.env.NODE_ENV === 'PRODUCTION') DB_URI = process.env.DB_URI;
+        
+        await mongoose.connect(DB_URI, {});
 
         await Product.deleteMany();
         await Product.insertMany(products);
