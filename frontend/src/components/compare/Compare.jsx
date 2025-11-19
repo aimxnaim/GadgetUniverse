00e451fd881e706fb76ce39274783c5fad8a66f6
Aiman Naim <aimxnaim@gmail.com>
2025-11-19 19:24:01 +0800

    Add under development notices for wishlist and compare

--- frontend/src/components/compare/Compare.jsx
+++ frontend/src/components/compare/Compare.jsx
@@ -1,67 +1,14 @@
 import React from 'react'
 import MetaData from '../layout/MetaData'
 import BreadCrumb from '../store/BreadCrumb'
-import StarRatings from 'react-star-ratings'
+import UnderDevelopment from '../common/UnderDevelopment'
 
 const Compare = () => {
     return (
         <>
             <MetaData title={'Compare'} />
             <BreadCrumb title='Compare Product' />
-            <div className="compare-product-wrapper home-wrapper-2 py-5">
-                <div className="container-xxl">
-                    <div className="row">
-                        {[1, 2, 3, 4].map((item, index) => (
-                            <>
-                                <div className="col-3" key={index}>
-                                    <div className="compare-product-card position-relative">
-                                        <img
-                                            src="images/youtube/cross.svg"
-                                            alt=""
-                                            className="position-absolute cross img-fluid"
-                                        />
-                                        <div className="compare-card-image">
-                                            <img src="images/youtube/home/smart watch.png" alt="watch" />
-                                        </div>
-                                        <div className="compare-product-details">
-                                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                                                <h5 className="title">
-                                                    Apple AirPods with Charging Case (Wired)
-                                                </h5>
-                                                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '8px' }}>
-                                                    <StarRatings
-                                                        rating={4}
-                                                        starRatedColor="#ffb829"
-                                                        numberOfStars={5}
-                                                        name="rating"
-                                                        starDimension="23px"
-                                                        starSpacing="0"
-                                                    />
-                                                </div>
-                                            </div>
-                                            <div>
-                                                <div className="product-detail" style={{ marginTop: '10px' }}>
-                                                    <h5>Price</h5>
-                                                    <h6>RM 100</h6>
-                                                </div>
-                                                <div className="product-detail">
-                                                    <h5>Availability</h5>
-                                                    <h6 className='greenColor'>In Stock</h6>
-                                                </div>
-                                                <div className="product-detail">
-                                                    <h5>Sold by</h5>
-                                                    <h6>Sony</h6>
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </>
-                        ))}
-                    </div>
-                </div>
-            </div>
+            <UnderDevelopment featureName='Product Comparison' />
         </>
     )
 }
--- frontend/src/wishlist/Wishlist.jsx
+++ frontend/src/wishlist/Wishlist.jsx
@@ -1,51 +1,14 @@
 import React from 'react'
 import MetaData from '../components/layout/MetaData'
 import BreadCrumb from '../components/store/BreadCrumb'
-import StarRatings from 'react-star-ratings'
-import { Link } from 'react-router-dom'
+import UnderDevelopment from '../components/common/UnderDevelopment'
 
 const Wishlist = () => {
     return (
         <>
             <MetaData title={'Favourite Wishlist'} />
             <BreadCrumb title='Favourite Wishlist' />
-            <div className="wishlist-wrapper home-wrapper-2 py-5">
-                <div className="container-xxl">
-                    <div className="row">
-                        {[1, 2, 3, 4].map((item, index) => (
-                            <>
-                                <div className="col-3" key={index}>
-                                    <div className="wishlist-card w-100 position-relative">
-                                        <img
-                                            src="images/youtube/cross.svg"
-                                            alt=""
-                                            className="position-absolute cross img-fluid"
-                                        />
-                                        <div className="wishlist-card-image">
-                                            <img src="images/youtube/home/smart watch.png" alt="watch" />
-                                        </div>
-                                        <div className="wishlist-product-details">
-                                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                                                <h5 className="title">
-                                                    Apple AirPods with Charging Case (Wired)
-                                                </h5>
-                                                <h6 className="price">
-                                                    RM 100
-                                                </h6>
-                                            </div>
-                                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                                                <Link className="wishlist-button">BUY NOW</Link>
-                                            </div>
-
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </>
-                        ))}
-                    </div>
-                </div>
-            </div>
+            <UnderDevelopment featureName='Wishlist & Favourites' />
         </>
     )
 }
