4294cae22dc113321a7c44a912c2e846077d4aa1
Aiman Naim <aimxnaim@gmail.com>
2025-11-19 19:25:09 +0800

    Preserve wishlist and compare layouts for future development

--- frontend/src/components/compare/Compare.jsx
+++ frontend/src/components/compare/Compare.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MetaData from '../layout/MetaData'
 import BreadCrumb from '../store/BreadCrumb'
 import UnderDevelopment from '../common/UnderDevelopment'
+import StarRatings from 'react-star-ratings'
 
 const Compare = () => {
     return (
@@ -9,6 +10,63 @@ const Compare = () => {
             <MetaData title={'Compare'} />
             <BreadCrumb title='Compare Product' />
             <UnderDevelopment featureName='Product Comparison' />
+            {/**
+             * Previous compare layout preserved for future implementation.
+             * Set the condition to true and connect to real data once comparison is available.
+             */}
+            {false && (
+                <div className="compare-product-wrapper home-wrapper-2 py-5">
+                    <div className="container-xxl">
+                        <div className="row">
+                            {[1, 2, 3, 4].map((item, index) => (
+                                <div className="col-3" key={index}>
+                                    <div className="compare-product-card position-relative">
+                                        <img
+                                            src="images/youtube/cross.svg"
+                                            alt=""
+                                            className="position-absolute cross img-fluid"
+                                        />
+                                        <div className="compare-card-image">
+                                            <img src="images/youtube/home/smart watch.png" alt="watch" />
+                                        </div>
+                                        <div className="compare-product-details">
+                                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                                <h5 className="title">
+                                                    Apple AirPods with Charging Case (Wired)
+                                                </h5>
+                                                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '8px' }}>
+                                                    <StarRatings
+                                                        rating={4}
+                                                        starRatedColor="#ffb829"
+                                                        numberOfStars={5}
+                                                        name="rating"
+                                                        starDimension="23px"
+                                                        starSpacing="0"
+                                                    />
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <div className="product-detail" style={{ marginTop: '10px' }}>
+                                                    <h5>Price</h5>
+                                                    <h6>RM 100</h6>
+                                                </div>
+                                                <div className="product-detail">
+                                                    <h5>Availability</h5>
+                                                    <h6 className='greenColor'>In Stock</h6>
+                                                </div>
+                                                <div className="product-detail">
+                                                    <h5>Sold by</h5>
+                                                    <h6>Sony</h6>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
--- frontend/src/wishlist/Wishlist.jsx
+++ frontend/src/wishlist/Wishlist.jsx
@@ -9,6 +9,45 @@ const Wishlist = () => {
             <MetaData title={'Favourite Wishlist'} />
             <BreadCrumb title='Favourite Wishlist' />
             <UnderDevelopment featureName='Wishlist & Favourites' />
+            {/**
+             * Previous wishlist grid preserved for future implementation.
+             * Uncomment and integrate with real data once wishlist functionality is ready.
+             */}
+            {false && (
+                <div className="wishlist-wrapper home-wrapper-2 py-5">
+                    <div className="container-xxl">
+                        <div className="row">
+                            {[1, 2, 3, 4].map((item, index) => (
+                                <div className="col-3" key={index}>
+                                    <div className="wishlist-card w-100 position-relative">
+                                        <img
+                                            src="images/youtube/cross.svg"
+                                            alt=""
+                                            className="position-absolute cross img-fluid"
+                                        />
+                                        <div className="wishlist-card-image">
+                                            <img src="images/youtube/home/smart watch.png" alt="watch" />
+                                        </div>
+                                        <div className="wishlist-product-details">
+                                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                                <h5 className="title">
+                                                    Apple AirPods with Charging Case (Wired)
+                                                </h5>
+                                                <h6 className="price">
+                                                    RM 100
+                                                </h6>
+                                            </div>
+                                            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                                                <a className="wishlist-button">BUY NOW</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
